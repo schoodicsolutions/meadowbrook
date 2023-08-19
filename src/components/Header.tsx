8144f075158b697dbb9b6cb4cd7044f762bbddb9
Ryan Prescott <rppgoat@gmail.com>
2023-08-18 23:46:37 -0400

    Responsive top stripe

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import TopStripe from './TopStripe';
 
 export default function Header() {
   return (
-    <header className="h-[132px] px-[167px] pt-2.5 pb-3 lg:relative">
+    <header className="h-[132px] lg:px-[167px] pt-2.5 pb-3 lg:relative">
       <TopStripe />
       <Logo />
     </header>
--- src/components/TopStripe.tsx
+++ src/components/TopStripe.tsx
@@ -1,8 +1,8 @@
 export default function TopStripe() {
     return (
-        <div className="flex absolute top-0 right-0">
-            <div className="border-transparent border-b-scarlet border-r-scarlet border-[21px] w-0 h-0" />
-            <div className="bg-scarlet h-[42px] px-[44px] flex gap-12 text-white font-bold items-center">
+        <div className="flex w-full lg:absolute lg:top-0 lg:right-0 lg:w-fit">
+            <div className="border-transparent border-b-scarlet border-r-scarlet lg:border-[21px] w-0 h-0" />
+            <div className="bg-scarlet h-[42px] px-[44px] flex gap-12 text-white font-bold items-center grow">
                 <p>888 Main Street, East Machias, ME 04630</p>
                 <p>(207) 259-6068</p>
             </div>
