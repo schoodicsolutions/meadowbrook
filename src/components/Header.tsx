e217fdd9711fae07e2fe8cc595db88e9b0ba65ff
Ryan Prescott <rppgoat@gmail.com>
2023-08-18 22:51:45 -0400

    Switch to t3 app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,1 +1,9 @@
-<header className="h-[132px] lg:relative"></header>;
+import { Logo } from './Logo.tsx';
+
+export default function Header() {
+  return (
+    <header className="h-[132px] px-[167px] lg:relative">
+      <Logo />
+    </header>
+  );
+}
