bc09cc979ffd0293728a45e2de944cae50f89600
SNAKE <darkmoou@gmail.com>
2025-08-08 00:33:31 +0500

    Fixed About / Sidebar form

--- src/Data/index.js
+++ src/Data/index.js
@@ -1,124 +1,120 @@
 const navLinks = [
-    {
-        id: "/",
-        title: "Home",
-    },
-    {
-        id: "/about-us",
-        title: "About Us",
-    },
-    {
-        id: '/concrete',
-        title: "Concrete",
-        submenu: [
-            {
-                id: '/concrete/redi-mix-concrete',
-                title: 'Redi-Mix Concrete'
-            },
-            {
-                id: '/concrete/pre-cast-concrete',
-                title: 'Pre-Cast Concrete'
-            }
-        ]
-    },
-    {
-        id: '/construction',
-        title: "Construction",
-        submenu: [
-            {
-                id: '/construction/driveways',
-                title: 'Driveways'
-            },
-            {
-                id: '/construction/culverts',
-                title: 'Culverts'
-            },
-            {
-                id: '/construction/dump-trucks',
-                title: 'Dump Trucks'
-            },
-            {
-                id: '/construction/excavator-services',
-                title: 'Excavator Services'
-            },
-            {
-                id: '/construction/septic-tanks',
-                title: 'Septic Tanks'
-            }
-        ]
-    },
-    {
-        id: '/materials',
-        title: "Materials",
-        submenu: [
-            {
-                id: '/materials/stones',
-                title: "Stone",
-            },
-            {
-                id: '/materials/gravel',
-                title: "Gravel",
-            },
-            {
-                id: '/materials/soil',
-                title: "Soil",
-            },
-        ],
-    },
-    {
-        id: '/careers',
-        title: "Careers",
-    },
-    {
-        id: "#ContactUs",
-        title: "Contact Us",
-    },
+  {
+    id: "/",
+    title: "Home",
+  },
+  {
+    id: "/about-us",
+    title: "About Us",
+  },
+  {
+    id: "/concrete",
+    title: "Concrete",
+    submenu: [
+      {
+        id: "/concrete/redi-mix-concrete",
+        title: "Redi-Mix Concrete",
+      },
+      {
+        id: "/concrete/pre-cast-concrete",
+        title: "Pre-Cast Concrete",
+      },
+    ],
+  },
+  {
+    id: "/construction",
+    title: "Construction",
+    submenu: [
+      {
+        id: "/construction/driveways",
+        title: "Driveways",
+      },
+      {
+        id: "/construction/culverts",
+        title: "Culverts",
+      },
+      {
+        id: "/construction/dump-trucks",
+        title: "Dump Trucks",
+      },
+      {
+        id: "/construction/excavator-services",
+        title: "Excavator Services",
+      },
+      {
+        id: "/construction/septic-tanks",
+        title: "Septic Tanks",
+      },
+    ],
+  },
+  {
+    id: "/materials",
+    title: "Materials",
+    submenu: [
+      {
+        id: "/materials/stones",
+        title: "Stone",
+      },
+      {
+        id: "/materials/gravel",
+        title: "Gravel",
+      },
+      {
+        id: "/materials/soil",
+        title: "Soil",
+      },
+    ],
+  },
+  {
+    id: "/careers",
+    title: "Careers",
+  },
+  {
+    id: "#ContactUs",
+    title: "Contact Us",
+  },
 ];
 
 const ProductLinks = [
-    {
-        id: '/materials/stones',
-        title: "Stone"
-    },
-    {
-        id: '/materials/gravel',
-        title: "Gravel"
-    },
-    {
-        id: '/materials/soil',
-        title: "Soil"
-    },
-    {
-        id: '/construction',
-        title: "Construction"
-    }
-]
+  {
+    id: "/materials/stones",
+    title: "Stone",
+  },
+  {
+    id: "/materials/gravel",
+    title: "Gravel",
+  },
+  {
+    id: "/materials/soil",
+    title: "Soil",
+  },
+  {
+    id: "/construction",
+    title: "Construction",
+  },
+];
 
 const Blogs = [
-    {
-        id: 1,
-        link: "/",
-        title: "How to get easist and cheapest quality of marble",
-    },
-    {
-        id: 2,
-        link: "/",
-        title: "10 Ways to start the construction business in no time",
-    },
-    {
-        id: 3,
-        link: "/",
-        title: "5 myths busters in Landscaping Service",
-    },
-    {
-        id: 4,
-        link: "/",
-        title: "Top 11 ways to back out the old building",
-    },
-]
+  {
+    id: 1,
+    link: "/",
+    title: "How to get easist and cheapest quality of marble",
+  },
+  {
+    id: 2,
+    link: "/",
+    title: "10 Ways to start the construction business in no time",
+  },
+  {
+    id: 3,
+    link: "/",
+    title: "5 myths busters in Landscaping Service",
+  },
+  {
+    id: 4,
+    link: "/",
+    title: "Top 11 ways to back out the old building",
+  },
+];
 
-export {
-    navLinks,
-    ProductLinks,
-    Blogs
-};
+export { navLinks, ProductLinks, Blogs };
