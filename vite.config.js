9a91369e2d07c1b92e2022f34949aa7f6fb0f16c
SNAKE <darkmoou@gmail.com>
2025-08-08 05:44:36 +0500

    New tab bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
       include: "**/*.{js,ts,jsx,tsx}",
     }),
-    
   ],
-})
+  server: {
+    historyApiFallback: true, // 👈 fixes local refresh/new tab
+  },
+});
